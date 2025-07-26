34aa902223ada8d436da446d315816dd8b8d2c4a
jjoaotorquato <torquatojoao57@gmail.com>
2025-07-26 11:30:13 -0300

    par impar

---
+++ src/ex5_ParOuImpar.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class ex5_ParOuImpar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Informe numero: ");
+
+        int num = scanner.nextInt();
+        
+        if(num %2 == 0){
+            System.out.println(num+" é par");
+        }else{
+            System.out.println(num+" é impar");
+        }
+    }
+}
