6a659ea00db40750429796f9dc68693b0c7eaad6
jjoaotorquato <torquatojoao57@gmail.com>
2025-07-26 11:23:11 -0300

    ex4 media

---
+++ ex4_Media.java
@@ -0,0 +1,10 @@
+public class ex4_Media {
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 3;
+        int num3 = 5;
+        int soma = num1 + num2 + num3;
+        int media = soma / 3;
+        System.out.println(media);
+    }
+}
