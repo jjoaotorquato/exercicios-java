c577aff1556672da7989cfe83714151a802edf9e
jjoaotorquato <torquatojoao57@gmail.com>
2025-08-10 20:48:42 -0300

    qntd letra

---
+++ src/ex23_QNTDletra.java
@@ -0,0 +1,6 @@
+public class ex23_QNTDletra {
+    public static void main(String[] args) {
+        String palavra = ("qunatas letras tem nessa string");
+        System.out.println(palavra.length());
+    }
+}
